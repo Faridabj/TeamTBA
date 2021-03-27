3b439a3370b1f049e1bef26af485bf38f01086cb
Faridabj <73083044+Faridabj@users.noreply.github.com>
2021-03-27 11:22:31 +0000

    Update datavis_farid.py

--- datavis_farid.py
+++ datavis_farid.py
@@ -26,5 +26,7 @@ app.layout = html.Div(
 )
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 #"""
